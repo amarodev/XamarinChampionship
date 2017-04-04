0115d7e64fed2ee66487423069c43872f3136c94
Abel Amaro Julian <jumaroz@live.com>
2017-04-03 20:17:13 -0500

    Reto 2 completed

--- Reto2/Start/Reto2/RegistroActivity.cs
+++ Reto2/Start/Reto2/RegistroActivity.cs
@@ -1,13 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Reto2.Services;
 
@@ -20,7 +13,38 @@ namespace Reto2
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Registro);
+            FindViewById<Button>(Resource.Id.btnRegistro).Click += RegistroActivity_Click;
         }
 
+        private async void RegistroActivity_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var serviceHelper = new ServiceHelper();
+                // Retrieve the values the user entered into the UI
+                var email = FindViewById<EditText>(Resource.Id.editTextEmail).Text;
+                var reto = Intent.GetStringExtra("Reto");
+                var androidId = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+
+                if (string.IsNullOrEmpty(reto)|| string.IsNullOrWhiteSpace(email))
+                {
+                    Toast.MakeText(this, "Por favor introduce un correo electrónico válido", ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Enviando tu registro", ToastLength.Short).Show();
+                    await serviceHelper.InsertarEntidad(email, reto, androidId);
+                    Toast.MakeText(this, "Gracias por registrarte", ToastLength.Long).Show();
+                    SetResult(Result.Ok, Intent);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                Toast.MakeText(this, exc.Message, ToastLength.Long).Show();
+                SetResult(Result.Canceled, Intent);
+            }
+            Finish();
+        }
     }
 }
--- Reto2/Start/Reto2/Resources/Resource.Designer.cs
+++ Reto2/Start/Reto2/Resources/Resource.Designer.cs
@@ -63,14 +63,23 @@ namespace Reto2
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050007
+			public const int btnRegistro = 2131034119;
+			
 			// aapt resource value: 0x7f050002
 			public const int btnSiguiente = 2131034114;
+			
+			// aapt resource value: 0x7f050006
+			public const int editTextEmail = 2131034118;
 			
 			// aapt resource value: 0x7f050001
 			public const int imageViewCinturon = 2131034113;
 			
 			// aapt resource value: 0x7f050003
 			public const int textViewAvisoPrivacidad = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int textViewEmail = 2131034117;
 			
 			// aapt resource value: 0x7f050000
 			public const int textViewResult = 2131034112;
